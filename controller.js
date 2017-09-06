768844f8d61226ea687a7a09e5c4b793505b9575
Stewart Atwood <stewartatwood@Stewarts-MacBook-Pro.local>
2017-09-06 13:59:59 -0500

    not sure if upload or commiting files

--- controller.js
+++ controller.js
@@ -1,10 +1,12 @@
-angular.module('userProfiles').controller('MainController', function($scope, mainService) {
+angular
+  .module("userProfiles")
+  .controller("MainController", function($scope, mainService) {
+    $scope.getUsers = function() {
+      mainService.getUsers().then(function(resp) {
+        console.log(resp);
+        $scope.users = resp;
+      });
+    };
 
-  $scope.getUsers = function() {
-  	$scope.users = mainService.getUsers();
-  }
-
-
-  $scope.getUsers();
-
-});
+    $scope.getUsers();
+  });
